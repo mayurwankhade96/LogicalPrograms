5b080f91fb65e011d2f44accb9a0c333c103a06f
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-05-28 01:30:24 +0530

    [Mayur] Add : FibonacciSeries class added

---
+++ LogicalPrograms/LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class FibonacciSeries
+    {
+        public int firstNumber = 0, secondNumber = 1, thirdNumber;
+        public void Fibonacci()
+        {
+            Console.WriteLine("Enter the number of elements : ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(firstNumber + "\n" + secondNumber);
+
+            for (int i=2; i<N; i++)
+            {
+                thirdNumber = firstNumber + secondNumber;
+                Console.WriteLine(thirdNumber);
+                firstNumber = secondNumber;
+                secondNumber = thirdNumber;
+            }
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -7,6 +7,19 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to logical programs.");
+
+            const int fibonacci = 1;
+
+            Console.WriteLine("Enter a class number : ");
+            int classNumber = Convert.ToInt32(Console.ReadLine());
+
+            switch (classNumber)
+            {
+                case fibonacci:
+                    FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                    fibonacciSeries.Fibonacci();
+                    break;
+            }
         }
     }
 }
